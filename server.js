60e1f959b68d6ac2584b9ce8adfccfba586835a7
Sandeep <sandeepwadhawan2001@gmail.com>
2025-12-01 21:44:35 +0530

    Socket.io code is setup as starter template

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+
 const app = require("./src/app");
 const connectDB = require('./src/db/db');
 
 connectDB();
 
+// Express server communicates via http protocol
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on('connection', (socket) => {
+        
+});
+
 const PORT = 3000;
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`);
 });
--- src/app.js
+++ src/app.js
@@ -2,4 +2,7 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("Started with GPT project");
+});
 module.exports = app;
